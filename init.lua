625391ee34e5a6313c300dfcef93184d5307fdf3
ksmilga <smilga.kaspars@gmail.com>
2023-12-23 13:55:57 +0200

    feat: Camel case word motion respect default ciw

--- init.lua
+++ init.lua
@@ -1,19 +1,25 @@
-require('keys')
-require('set')
+require("keys")
+require("set")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup("plugins")
+require("lazy").setup("plugins")
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { focusable = false })
+
+vim.api.nvim_create_autocmd("ExitPre", {
+	callback = function()
+		vim.cmd(":NvimTreeClose")
+	end,
+})
--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -1,8 +1,8 @@
 return {
-    "olimorris/persisted.nvim",
-    config = function()
-        require('persisted').setup({
-            autoload = true
-        })
-    end
+	"olimorris/persisted.nvim",
+	config = function()
+		require("persisted").setup({
+			autoload = true,
+		})
+	end,
 }
