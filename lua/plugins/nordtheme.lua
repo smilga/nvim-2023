777aca4c1bfd488cb9e7a640b41fe2d722f22213
ksmilga <smilga.kaspars@gmail.com>
2024-02-19 15:37:59 +0200

    feat: Theme updates

--- lua/plugins/nordtheme.lua
+++ lua/plugins/nordtheme.lua
@@ -7,6 +7,24 @@ return {
 		require("nord").setup({
 			transparent = false,
 			diff = { mode = "fg" },
+			on_highlights = function(highlights, colors)
+				colors.polar_night.origin = "#2E3440"
+				highlights["Normal"] = {
+					bg = "#21252e",
+				}
+				highlights["SignColumn"] = {
+					bg = "#21252e",
+				}
+				highlights["TelescopeBorder"] = {
+					bg = "#21252e",
+				}
+				highlights["NormalFloat"] = {
+					bg = "#21252e",
+				}
+				highlights["FloatBorder"] = {
+					bg = "#21252e",
+				}
+			end,
 		})
 		vim.cmd.colorscheme("nord")
 	end,
